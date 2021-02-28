2e942182886b13bef05a791b559d4f5fb4bf2086
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-02-28 18:25:20 +0000

    start of current_board spec added

---
+++ spec/board_spec.rb
@@ -0,0 +1,13 @@
+require_relative '../lib/board'
+
+describe GameBoard do
+  subject(:gameboard) { GameBoard.new }
+
+  describe '#current_board' do
+    context 'when called' do
+      xit 'returns the current board' do
+        # TODO
+      end
+    end
+  end
+end
