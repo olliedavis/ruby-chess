5431fee788f1fae071ec484472a5f2bc9ca023ec
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-01 19:09:34 +0000

    Switched Cnicode colors

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,14 +1,17 @@
+# technically the color of the unicode characters is the other way around
+# but terminal shows black as opaque white, and white as transparent
+# which just makes it confusing as most terminals are black or purple, it make sense to be rotated.
 module Pieces
-  WHITE_KING = "\u2654",
-  WHITE_QUEEN = "\u2655",
-  WHITE_ROOK = "\u2656",
-  WHITE_BISHOP = "\u2657",
-  WHITE_KNIGHT = "\u2658",
-  WHITE_PAWN = "\u2659",
-  BLACK_KING = "\u265A",
-  BLACK_QUEEN = "\u265B",
-  BLACK_ROOK = "\u265C",
-  BLACK_BISHOP = "\u265D",
-  BLACK_KNIGHT = "\u265E",
-  BLACK_PAWN = "\u265F"
+  BLACK_KING = "\u2654"
+  BLACK_QUEEN = "\u2655"
+  BLACK_ROOK = "\u2656"
+  BLACK_BISHOP = "\u2657"
+  BLACK_KNIGHT = "\u2658"
+  BLACK_PAWN = "\u2659"
+  WHITE_KING = "\u265A"
+  WHITE_QUEEN = "\u265B"
+  WHITE_ROOK = "\u265C"
+  WHITE_BISHOP = "\u265D"
+  WHITE_KNIGHT = "\u265E"
+  WHITE_PAWN = "\u265F"
 end
