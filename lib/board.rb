c80a3e175d515e24c5f287a0ee32d40385366a7d
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-01 06:56:58 +0000

    Board created

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,19 @@
 class Chessboard
+  def initialize
+    @board = [' '] * 64
+    current_board
+  end
+
+  def current_board
+    puts "\n"
+    8.times.reverse_each do |y|
+      print "#{y + 1}|"
+      8.times do |n|
+        print " #{@board[n]} |"
+      end
+      puts "\n --------------------------------- "
+    end
+    puts ' | A | B | C | D | E | F | G | H |'
+    puts "\n"
+  end
 end
