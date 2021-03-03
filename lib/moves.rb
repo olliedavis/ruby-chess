e87e472625506e7d42110cee14ad8860bbec0a6a
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-03 07:08:14 +0000

    start of valid_moves redo

--- lib/moves.rb
+++ lib/moves.rb
@@ -8,75 +8,31 @@ require_relative 'pieces'
 class ValidMoves < Chessboard
   include Pieces
 
-  def current_row(piece)
-    @rows.each_with_index do |row, idx|
-      return idx if row.include?(piece)
-    end
+  def position_to_index(position_string)
+    positions = %w[a b c d e f g h]
+    column = positions.downcase.index(position_string[0])
+    row = position_string[1].to_i
+    [(8 - row), column]
   end
 
-  def current_column(piece)
-    @columns.each_with_index do |column, idx|
-      return idx if column.include?(piece)
-    end
-  end
-
-  def king(color)
-    if color == black
-      current_column(BLACK_KING)
-      current_row(BLACK_KING)
-    else
-      current_column(WHITE_KING)
-      current_row(WHITE_KING)
-    end
+  def king(color, position)
+    position_idx = position_to_index(position)
+    valid_moves = position_idx[[-1, -1], [-1, 0], [-1, 1],[0, -1],[0, 1], [1, -1], [1, 0], [1, 1]]
   end
 
   def queen(color)
-    if color == black
-      current_column(BLACK_QUEEN)
-      current_row(BLACK_QUEEN)
-    else
-      current_column(WHITE_QUEEN)
-      current_row(WHITE_QUEEN)
-    end
   end
 
   def rook(color)
-    if color == black
-      current_column(BLACK_ROOK)
-      current_row(BLACK_ROOK)
-    else
-      current_column(WHITE_ROOK)
-      current_row(WHITE_ROOK)
-    end
   end
 
   def bishop(color)
-    if color == black
-      current_column(BLACK_BISHOP)
-      current_row(BLACK_BISHOP)
-    else
-      current_column(WHITE_BISHOP)
-      current_row(WHITE_BISHOP)
-    end
+
   end
 
   def knight(color)
-    if color == black
-      current_column(BLACK_KNIGHT)
-      current_row(BLACK_KNIGHT)
-    else
-      current_column(WHITE_KNIGHT)
-      current_row(WHITE_KNIGHT)
-    end
   end
 
   def pawn(color, identifier) 
-    if color == black
-      current_column(BLACK_PAWN)
-      current_row(BLACK_PAWN)
-    else
-      current_column(WHITE_PAWN)
-      current_row(WHITE_PAWN)
-    end
   end
 end
