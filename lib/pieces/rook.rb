c4f32020f9049275a9dff608f5ac507405e070c1
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-04 20:01:38 +0000

    Rook moves added

--- lib/pieces/rook.rb
+++ lib/pieces/rook.rb
@@ -1,6 +1,15 @@
 class Rook
   attr_reader :moves
   def initialize
-    @moves =
+    @moves =[
+      # up
+      (1..7).map { |n| [-n, 0] },
+      # down
+      (1..7).map { |n| [n, 0] },
+      # left
+      (1..7).map { |n| [0, -n] },
+      # right
+      (1..7).map { |n| [0, n] }
+    ]
   end
 end
