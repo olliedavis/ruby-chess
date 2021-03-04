724188ae76afc307a89a9d0ee070d7be93c8ee4b
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-04 08:02:58 +0000

    Removal of currently redundant code

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -1,10 +1,6 @@
-require_relative '../board'
-require_relative '../game'
 class King
   attr_reader :moves
   def initialize
     @moves = [[-1, -1], [-1, 0], [-1, 1],[0, -1],[0, 1], [1, -1], [1, 0], [1, 1]].freeze
   end
-
-  
 end
