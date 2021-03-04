160da608831498f2e653f9d74b659e170ee9e168
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-04 19:56:00 +0000

    queen moves added

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,6 +1,24 @@
 class Queen
   attr_reader :moves
+
   def initialize
-    @moves =
+    @moves = [
+      # up
+      (1..7).map { |n| [-n, 0] },
+      # down
+      (1..7).map { |n| [n, 0] },
+      # left
+      (1..7).map { |n| [0, -n] },
+      # right
+      (1..7).map { |n| [0, n] },
+      # Up Left Diagonally
+      (1..7).map { |n| [-n, -n] },
+      # Up Right Diagonally
+      (1..7).map { |n| [-n, n] },
+      # Down Left Diagonally
+      (1..7).map { |n| [n, -n] },
+      # Down Right Diagonally
+      (1..7).map { |n| [n, n] }
+    ]
   end
 end
