637ec99056399fbcacf348e09e008c6d706f1cbc
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-04 19:46:38 +0000

    corrected move length total

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,10 +1,10 @@
 class Bishop
   attr_reader :moves
   def initialize
-    up_left_diag = (1..8).map { |n| [-n, -n] }
-    up_right_diag = (1..8).map { |n| [-n, n] }
-    down_left_diag = (1..8).map { |n| [n, -n] }
-    down_right_diag = (1..8).map { |n| [n, n] }
+    up_left_diag = (1..7).map { |n| [-n, -n] }
+    up_right_diag = (1..7).map { |n| [-n, n] }
+    down_left_diag = (1..7).map { |n| [n, -n] }
+    down_right_diag = (1..7).map { |n| [n, n] }
     @moves = [up_left_diag, up_right_diag, down_left_diag, down_right_diag]
   end
 end
