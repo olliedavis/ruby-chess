668689ef66e0dd71836d97fbfc6f01a7a39685ed
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-04 14:03:37 +0000

    bishop moves created

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -1,7 +1,12 @@
 class Bishop
   attr_reader :moves
   def initialize
-    up_left_diag = (-8..-1).zip(-8..-1).reverse
-    @moves = [up_left_diag]
+    up_left_diag = (1..8).map { |n| [-n, -n] }
+    up_right_diag = (1..8).map { |n| [-n, n] }
+    down_left_diag = (1..8).map { |n| [n, -n] }
+    down_right_diag = (1..8).map { |n| [n, n] }
+    @moves = [up_left_diag, up_right_diag, down_left_diag, down_right_diag]
   end
 end
+
+Bishop.new
