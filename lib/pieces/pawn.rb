75a306c94670cc8d0dae3d0b867d173ca336bdbf
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-04 20:11:43 +0000

    Pawn moves added

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,6 +1,9 @@
 class Pawn
   attr_reader :moves
   def initialize
-    @moves =
+    @white_first_moves = [[-1, 0], [-2, 0]]
+    @black_first_moves = [[1, 0], [2, 0]]
+    @white_moves = [-1, 0]
+    @black_moves = [1, 0]
   end
 end
