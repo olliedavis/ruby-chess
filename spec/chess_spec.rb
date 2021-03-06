bb890133c3984e8c9a7e32ad42e76ded06b6a57a
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-06 10:43:11 +0000

    game_spec renamed, variables updated to match

--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,17 +1,17 @@
-require_relative '../lib/game'
+require_relative '../lib/chess'
 
-describe Game do
-  subject(:game) { described_class.new }
+describe Chess do
+  subject(:chess) { described_class.new }
 
   describe '#valid_position?' do
     context 'when a valid input is provided' do
       it 'returns true' do
-        expect(game.valid_position?('7B')).to be true
+        expect(chess.valid_position?('7B')).to be true
       end
     end
     context 'when an invalid input is provided' do
       it 'returns false' do
-        expect(game.valid_position?('72')).to be false
+        expect(chess.valid_position?('72')).to be false
       end
     end
   end
@@ -19,12 +19,12 @@ describe Game do
   describe '#valid_piece?' do
     context 'when a valid piece is selected' do
       it 'returns true' do
-        expect(game.valid_piece?('rook')).to be true
+        expect(chess.valid_piece?('rook')).to be true
       end
     end
     context 'when a valid piece is selected' do
       it 'returns false' do
-        expect(game.valid_piece?('rook7B')).to be false
+        expect(chess.valid_piece?('rook7B')).to be false
       end
     end
   end
@@ -32,13 +32,13 @@ describe Game do
   describe '#current_player' do
     context "when it is White's turn" do
       it 'returns White' do
-        expect(game.current_player(2)).to eq('white')
+        expect(chess.current_player(2)).to eq('white')
       end
     end
 
     context "when it is Black's turn" do
       it 'returns Black' do
-        expect(game.current_player(5)).to eq('black')
+        expect(chess.current_player(5)).to eq('black')
       end
     end
   end
