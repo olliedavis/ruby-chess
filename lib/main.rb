391d4d4803c5b26aae30ead300ffda728f2cc1ad
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-06 14:34:14 +0000

    Introduction created

--- lib/main.rb
+++ lib/main.rb
@@ -1,7 +1,25 @@
 require_relative 'chess'
 
 class Game
-  def initialize
-    @chess = Chess.new
+  def self.start
+    introduction
+    Chess.new.start_game
+  end
+
+  def self.introduction
+    puts 'Welcome to chess!'
+    puts ''
+    puts 'Two players will take it in turn to select a piece, and the position they want to move it to.'
+    puts 'When prompted, please enter the name of the piece you want to select (A reminder can be found in the README)'
+    puts 'You will then be prompted again for a location to move the above piece to.'
+    puts ''
+    puts "Ready? Please enter 'Start'"
+    prompt = gets.chomp.downcase
+    until prompt == 'start'
+      puts "Ready? Please enter 'Start'"
+      prompt = gets.chomp.downcase
+    end
   end
 end
+
+Game.start
