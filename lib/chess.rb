5a5c1f8fb31cebc63459742c27f2c4b4cd2362e1
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-07 19:20:02 +0000

    enter position' changed to 'enter coordinates'

--- lib/chess.rb
+++ lib/chess.rb
@@ -11,8 +11,8 @@ class Chess
   end
 
   def choose_piece_input
-    puts "White's turn! Please enter the position of the piece you want to move" if @turn_counter.even?
-    puts "Black's turn! Please enter the position of the piece you want to move" if @turn_counter.odd?
+    puts "White's turn! Please enter the coordinates of the piece you want to move" if @turn_counter.even?
+    puts "Black's turn! Please enter the coordinates of the piece you want to move" if @turn_counter.odd?
     piece = gets.chomp
     until valid_piece?(piece)
       puts 'Are you sure that position contains one of your pieces? Please try again.'
