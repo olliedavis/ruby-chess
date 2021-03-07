d1892859ca5094179f7f07b4eabb40377e6accd7
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-07 19:45:46 +0000

    color changed to piece

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,17 +1,17 @@
 class Pawn
   attr_reader :moves
 
-  def initialize(current_position, color)
-    @moves = first_move_check(current_position, color)
+  def initialize(current_position, piece)
+    @moves = first_move_check(current_position, piece)
   end
 
-  def first_move_check(position, color)
+  def first_move_check(position, piece)
     if position[0] == 1 || position[0] == 6
-      return [[-1, 0], [-2, 0]].freeze if color == 'white'
-      return [[1, 0], [2, 0]].freeze if color == 'black'
+      return [[-1, 0], [-2, 0]].freeze if piece == '♟'
+      return [[1, 0], [2, 0]].freeze if color == '♙'
     else
-      return [-1, 0].freeze if color == 'white'
-      return [1, 0].freeze if color == 'black'
+      return [-1, 0].freeze if piece == '♟'
+      return [1, 0].freeze if color == '♙'
     end
   end
 end
