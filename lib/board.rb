11dee5f453759e949409bbf525418db1830c9d59
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-16 18:54:37 +0000

    removed input_to_index now that it's in the converter module

--- lib/board.rb
+++ lib/board.rb
@@ -33,15 +33,7 @@ class Chessboard
     puts ' | A | B | C | D | E | F | G | H |'
     puts "\n"
   end
-
-  #  converts the letter's position to integer position. EG, B3 = [5, 1]
-  def input_to_index(input)
-    x_axis = %w[A B C D E F G H]
-    column = x_axis.index(input[0]) # returns the index of the first value of input
-    row = input[1].to_i
-    [(8 - row), column]
-  end
-
+  
   def position_taken?(new_position)
     return true if @board[new_position] != ' '
 
