228f015ac2a64b75acaee0030e532f907fd6af0f
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-17 07:51:43 +0000

    validator mehtods migrated to module

--- lib/InputValidator_module.rb
+++ lib/InputValidator_module.rb
@@ -1,2 +1,41 @@
 module InputValidator
+  def first_choice_validator(input)
+    # returns to start if input is not valid
+    first_choice_input if valid_input?(input) == false || valid_piece?(input) == false 
+  end
+
+  def second_choice_validator(first_input, second_input)
+    # returns to 2nd choice input if input is not valid
+    second_choice_input if valid_input?(second_input) == false || legal_move?(first_input, second_input) == false 
+  end
+
+  def valid_input?(input)
+    return true if input.length == 2 && input[0].between?('A', 'H') && input[1].between?('1', '8')
+
+    puts 'Unrecognised input - Please try again like this: B2'
+    false
+  end
+
+  def valid_piece?(input)
+    # converts input to equivalent index and checks if that index matches a piece of their color
+    index = input_to_index(input) 
+    if @turn_counter.even? && @white_pieces.any? { |piece| @chessboard.board[index[0]][index[1]] == piece }
+      true
+    elsif @turn_counter.odd? && @black_pieces.any? { |piece| @chessboard.board[index[0]][index[1]] == piece }
+      true
+    else
+      puts 'Unrecognised Piece - Please try again'
+      false
+    end
+  end
+
+  def legal_move?(first_input, second_input)
+    piece_class = input_to_class(first_input) # converts the first input to a class
+    original_position = input_to_index(first_input) # converts the first input to index
+    new_position = input_to_index(second_input) # converts the second input to index
+    piece_class.moves.each do |x, y| # returns true if new position matches any of the piece's move set
+      return true if new_position == [(original_position[0] + x), (original_position[1] + y)]
+    end
+    false # else false
+  end
 end
--- lib/chess.rb
+++ lib/chess.rb
@@ -1,10 +1,13 @@
 require_relative 'board'
 require_relative 'pieces'
 require_relative 'converter_module'
+require_relative 'InputValidator_module'
 Dir['../lib/pieces/*.rb'].sort.each { |file| require file }
 class Chess
   include Pieces
   include Converter
+  include InputValidator
+
   def initialize
     @chessboard = Chessboard.new
     @black_pieces = ['♖', '♘', '♗', '♕', '♔', '♙']
@@ -38,45 +41,5 @@ class Chess
   def move_piece_input
     puts 'Please enter the coordinates of where you would like to move your piece'
     gets.upcase.chomp
-  end
-
-  def first_choice_validator(input)
-    # returns to start if input is not valid
-    first_choice_input if valid_input?(input) == false || valid_piece?(input) == false 
-  end
-
-  def second_choice_validator(first_input, second_input)
-    # returns to 2nd choice input if input is not valid
-    second_choice_input if valid_input?(second_input) == false || legal_move?(first_input, second_input) == false 
-  end
-
-  def valid_input?(input)
-    return true if input.length == 2 && input[0].between?('A', 'H') && input[1].between?('1', '8')
-
-    puts 'Unrecognised input - Please try again like this: B2'
-    false
-  end
-
-  def valid_piece?(input)
-    # converts input to equivalent index and checks if that index matches a piece of their color
-    index = input_to_index(input) 
-    if @turn_counter.even? && @white_pieces.any? { |piece| @chessboard.board[index[0]][index[1]] == piece }
-      true
-    elsif @turn_counter.odd? && @black_pieces.any? { |piece| @chessboard.board[index[0]][index[1]] == piece }
-      true
-    else
-      puts 'Unrecognised Piece - Please try again'
-      false
-    end
-  end
-
-  def legal_move?(first_input, second_input)
-    piece_class = input_to_class(first_input) # converts the first input to a class
-    original_position = input_to_index(first_input) # converts the first input to index
-    new_position = input_to_index(second_input) # converts the second input to index
-    piece_class.moves.each do |x, y| # returns true if new position matches any of the piece's move set
-      return true if new_position == [(original_position[0] + x), (original_position[1] + y)]
-    end
-    false
   end
 end
