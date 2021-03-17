d7183e4353fc206e243df8409d1e4824fdc42a50
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-17 07:19:47 +0000

    parameters switched around

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,11 +1,11 @@
 class Pawn
   attr_reader :moves
 
-  def initialize(current_position, piece)
-    @moves = first_move_check(current_position, piece)
+  def initialize(piece, current_position)
+    @moves = first_move_check(piece, current_position)
   end
 
-  def first_move_check(position, piece)
+  def first_move_check(piece, position)
     if position[0] == 1 || position[0] == 6
       return [[-1, 0], [-2, 0]].freeze if piece == '♟'
       return [[1, 0], [2, 0]].freeze if piece == '♙'
