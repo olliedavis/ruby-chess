eb463ec4b4bb72ad90515f43a7f625d28cbd79ba
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-19 07:58:28 +0000

    new_space_free? method added

--- lib/InputValidator_module.rb
+++ lib/InputValidator_module.rb
@@ -1,12 +1,13 @@
 module InputValidator
   def first_choice_validator(input)
     # returns to start if input is not valid
-    first_choice_input if valid_input?(input) == false || valid_piece?(input) == false 
+    first_choice_input if valid_input?(input) == false || valid_piece?(input) == false
   end
 
   def second_choice_validator(first_input, second_input)
     # returns to 2nd choice input if input is not valid
-    second_choice_input if valid_input?(second_input) == false || legal_move?(first_input, second_input) == false 
+    second_choice_input(first_input) if valid_input?(second_input) == false || legal_move?(first_input,
+                                                                                           second_input) == false
   end
 
   def valid_input?(input)
@@ -18,7 +19,7 @@ module InputValidator
 
   def valid_piece?(input)
     # converts input to equivalent index and checks if that index matches a piece of their color
-    index = input_to_index(input) 
+    index = input_to_index(input)
     if @turn_counter.even? && @white_pieces.any? { |piece| @chessboard.board[index[0]][index[1]] == piece }
       true
     elsif @turn_counter.odd? && @black_pieces.any? { |piece| @chessboard.board[index[0]][index[1]] == piece }
@@ -34,8 +35,21 @@ module InputValidator
     original_position = input_to_index(first_input) # converts the first input to index
     new_position = input_to_index(second_input) # converts the second input to index
     piece_class.moves.each do |x, y| # returns true if new position matches any of the piece's move set
-      return true if new_position == [(original_position[0] + x), (original_position[1] + y)]
+      return true if new_position == [(original_position[0] + x),
+                                      (original_position[1] + y)] && new_space_free?(second_input)
     end
-    false # else false
+    false
+  end
+
+  def new_space_free?(second_input)
+    new_position = input_to_piece(second_input, @chessboard.board)
+    # new position doesn't contain own piece then it's free to move to
+    if @turn_counter.even? && @white_pieces.include?(new_position) == false
+      true
+    elsif @turn_counter.odd? && @black_pieces.include?(new_position) == false
+      true
+    else
+      false
+    end
   end
 end
