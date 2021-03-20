cc13e42ef674c2a2557a367266491b1b03dfd0d1
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-20 14:33:38 +0000

    Fixed bug where you couldn't move the pawn twice

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -7,11 +7,11 @@ class Pawn
 
   def first_move_check(piece, position)
     if position[0] == 1 || position[0] == 6
-      return [[-1, 0], [-2, 0]].freeze if piece == '♟'
-      return [[1, 0], [2, 0]].freeze if piece == '♙'
+      return [[-1, 0], [-2, 0]] if piece == '♟'
+      return [[1, 0], [2, 0]] if piece == '♙'
     else
-      return [-1, 0].freeze if piece == '♟'
-      return [1, 0].freeze if piece == '♙'
+      return [[-1, 0]] if piece == '♟'
+      return [[1, 0]] if piece == '♙'
     end
   end
 end
