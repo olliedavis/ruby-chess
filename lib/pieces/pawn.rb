ca6b9e74b24a9e2b9df3ecb9d1b6e8c501305391
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-20 15:48:16 +0000

    legal_diagonal? Added

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -1,17 +1,33 @@
 class Pawn
   attr_reader :moves
 
-  def initialize(piece, current_position)
-    @moves = first_move_check(piece, current_position)
+  def initialize(piece, first_index, second_index, board)
+    @black_pieces = ['♖', '♘', '♗', '♕', '♔', '♙']
+    @white_pieces = ['♜', '♞', '♝', '♚', '♛', '♟']
+    @moves = move_check(piece, first_index, second_index, board)
   end
 
-  def first_move_check(piece, position)
-    if position[0] == 1 || position[0] == 6
+  def move_check(piece, first_index, second_index, board)
+    if first_index[1] != second_index[1] && legal_diagonal?(piece, second_index, board)
+      return [[-1, -1], [-1, 1]] if piece == '♟'
+      return [[1, 1], [1, -1]] if piece == '♙'
+
+    elsif first_index[0] == 1 || first_index[0] == 6
       return [[-1, 0], [-2, 0]] if piece == '♟'
       return [[1, 0], [2, 0]] if piece == '♙'
     else
       return [[-1, 0]] if piece == '♟'
       return [[1, 0]] if piece == '♙'
+    end
+  end
+
+  def legal_diagonal?(piece, second_index, board)
+    if piece == '♟' && @black_pieces.any? { |black_piece| black_piece == board[second_index[0]][second_index[1]] }
+      true
+    elsif piece == '♙' && @white_pieces.any? { |white_piece| white_piece == board[second_index[0]][second_index[1]] }
+      true
+    else
+      false
     end
   end
 end
