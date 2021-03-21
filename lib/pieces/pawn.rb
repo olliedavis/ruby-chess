ee90061ac211981f5f0a4d62a2820d9180b5c3eb
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-21 14:02:02 +0000

    promotion? refined

--- lib/pieces/pawn.rb
+++ lib/pieces/pawn.rb
@@ -2,8 +2,8 @@ class Pawn
   attr_reader :moves
 
   def initialize(piece, first_index, second_index, board)
-    @black_pieces = ['♖', '♘', '♗', '♕', '♔', '♙']
-    @white_pieces = ['♜', '♞', '♝', '♚', '♛', '♟']
+    @black_pieces = ['♔', '♕', '♗', '♖', '♘', '♙']
+    @white_pieces = ['♚', '♛', '♝', '♜', '♞', '♟']
     @moves = move_check(piece, first_index, second_index, board)
   end
 
@@ -31,30 +31,24 @@ class Pawn
     end
   end
 
-  def promotion?(piece, second_index)
-    if piece == '♟' && second_index[0].zero?
-      promote_prompt
-    elsif piece == '♙' && second_index[0] == 7
-      promote_prompt
-    else
-      false
-    end
+  def promotion?(second_index)
+    return true if second_index[0].zero? || second_index[0] == 7
+
+    false
   end
 
-  def promote_prompt
+  def promote_choice(pawn)
     puts "Your Pawn has reached the opponent's back rank"
     puts 'This means you can promote your Pawn to any piece except King'
     puts 'Please enter the corresponding number of the piece you would like to promote it to'
     puts '1: Queen, 2: Bishop, 3: Rook, 4: Knight'
-    choice = gets.chomp.to_i
+    choice_int = gets.chomp.to_i
 
-    until choice.between?(1, 4)
+    until choice_int.between?(1, 4)
       puts 'Error. Please enter just the number of the piece you would like to promote your Pawn to'
       puts '1: Queen, 2: Bishop, 3: Rook, 4: Knight'
-      choice = gets.chomp.to_i
+      choice_int = gets.chomp.to_i
     end
+    promotion_choice_to_piece(pawn, choice_int)
   end
-
-
-
 end
