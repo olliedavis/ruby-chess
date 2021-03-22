580c20527ba9e5e71b2811352d466671c4425bf6
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-22 07:41:31 +0000

    Methods polished

--- lib/InCheck.rb
+++ lib/InCheck.rb
@@ -1,27 +1,29 @@
 module InCheck
-  def all_pieces_on_board
-    white_pieces_pos_array = []
-    black_pieces_pos_array = []
+  def all_pieces_on_board(color)
+    pos_array = []
     @chessboard.board.each_with_index do |row, row_idx|
       row.each_with_index do |square, square_idx|
-        if @white_pieces.any?(square)
-          white_pieces_pos_array << [row_idx, square_idx]
-        elsif @black_pieces.any?(square)
-          black_pieces_pos_array << [row_idx, square_idx]
+        if @white_pieces.any?(square) && color == 'white'
+          pos_array << [row_idx, square_idx]
+        elsif @black_pieces.any?(square) && color == 'black'
+          pos_array << [row_idx, square_idx]
         else
           next
         end
       end
     end
-    all_available_moves(white_pieces_pos_array, black_pieces_pos_array)
+    pos_array
   end
 
-  def all_available_moves(white_array, black_array)
-    white_array.each do |position|
+  def all_available_moves(pos_array)
+    available_moves = []
+    pos_array.each do |position|
       piece = index_to_piece(position)
-      if @pawn.any?(piece)
-        piece_class = piece_to_class(piece, position)
-      end
+      return available_moves.push(@pawn.possible_moves(position, @chessboard.board)) if @pawns.any?(piece)
+
+      piece_class = piece_to_class(piece)
+      piece_class.moves.each { |x, y| available_moves.push([(position[0] + x), (position[1] + y)]) }
     end
+    available_moves
   end
 end
