bef8cfbe693fd8d2ee3210ab5a7d2c0f801e2c10
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-24 07:17:59 +0000

    path mapping method changed

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -17,19 +17,18 @@ class Bishop
   def path(current_position, new_position)
     cur_x = current_position[0]
     cur_y = current_position[1]
-    new_x = new_position[0]
-    new_y = new_position[1]
-    x_diff = cur_x - new_x
-    y_diff = cur_y - new_y
+    x_diff = cur_x - new_position[0]
+    y_diff = cur_y - new_position[1]
+    path = []
 
     if x_diff.positive? && y_diff.positive?
-      (1..7).map { |n| [cur_x - n, cur_y - n] }
+      path.push([cur_x -= 1, cur_y -= 1]) until cur_x.zero? || cur_y.zero?
     elsif x_diff.positive? && y_diff.negative?
-      (1..7).map { |n| [cur_x - n, cur_y + n] }
+      path.push([cur_x -= 1, cur_y += 1]) until cur_x .zero? || cur_y == 7
     elsif x_diff.negative? && y_diff.positive?
-      (1..7).map { |n| [cur_x + n, cur_y - n] }
+      path.push([cur_x += 1, cur_y -= 1]) until cur_x == 7 || cur_y.zero?
     else
-      (1..7).map { |n| [cur_x + n, cur_y + n] }
+      path.push([cur_x += 1, cur_y += 1]) until cur_x == 7 || cur_y == 7
     end
   end
 end
