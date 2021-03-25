885ad018c941359a1380229d42c0fe7a64596800
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-25 19:45:49 +0000

    code cleanup

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -1,26 +1,23 @@
 require_relative '../modules/PathCreator'
-require_relative '../chess'
 
 class Queen
   attr_reader :moves
 
   include PathCreator
 
-  def initialize(current_position)
+  def initialize(current_position, board)
     all_paths = path_creator(current_position, all_moves)
-    @moves = path_checker(all_paths, chess.board)
+    @moves = path_checker(all_paths, board)
   end
 
   def all_moves
-    [
-      (1..7).map { |n| [-n, 0] }.freeze, # up
-      (1..7).map { |n| [n, 0] }.freeze, # down
-      (1..7).map { |n| [0, -n] }.freeze, # left
-      (1..7).map { |n| [0, n] }.freeze, # right
-      (1..7).map { |n| [-n, -n] }.freeze, # up left
-      (1..7).map { |n| [-n, n] }.freeze, # up right
-      (1..7).map { |n| [n, -n] }.freeze, # down left
-      (1..7).map { |n| [n, n] }.freeze # down right
-    ]
+    [(1..7).map { |n| [-n, 0] }.freeze,  # up
+     (1..7).map { |n| [n, 0] }.freeze,   # down
+     (1..7).map { |n| [0, -n] }.freeze,  # left
+     (1..7).map { |n| [0, n] }.freeze,   # right
+     (1..7).map { |n| [-n, -n] }.freeze, # up left
+     (1..7).map { |n| [-n, n] }.freeze,  # up right
+     (1..7).map { |n| [n, -n] }.freeze,  # down left
+     (1..7).map { |n| [n, n] }.freeze]   # down right
   end
 end
