7c263fc16c8a35db9da928bfcf4272f8df88d718
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-27 16:25:59 +0000

    Path_checker now allows you to take pieces but not move further

--- lib/modules/PathCreator.rb
+++ lib/modules/PathCreator.rb
@@ -1,5 +1,4 @@
 module PathCreator
-
   def path_creator(current_position, moves)
     all_paths_array = []
     direction_array = []
@@ -15,13 +14,19 @@ module PathCreator
     all_paths_array # returns the array of legal move arrays
   end
 
-  def path_checker(all_paths, board)
+  def path_checker(all_paths, board, colour)
     real_path = []
     all_paths.each do |direction|
       direction.each do |coords| # goes through path
-        break if board[coords[0]][coords[1]] != ' ' # breaks the path if it reaches a piece
+        square = board[coords[0]][coords[1]] # square becomes what ever is in that position on the board
+        break if colour == 'white' && @white_pieces.any?(square) # if it reaches one of the players own pieces, break
+        break if colour == 'black' && @black_pieces.any?(square)
 
-        real_path << coords # pushes the path it got to before it reached a piece
+        real_path << coords
+        # if it reaches one of the opposition pieces, break after adding the position to the path
+        # this then allows you to take the piece, but not go any further
+        break if colour == 'black' && @white_pieces.any?(square)
+        break if colour == 'white' && @black_pieces.any?(square)
       end
     end
     real_path
