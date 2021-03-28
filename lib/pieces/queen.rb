b5724867fd5320b8a591a1f3f3be4ac3192b4609
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-28 14:54:02 +0100

    reduced complexity of all_moves

--- lib/pieces/queen.rb
+++ lib/pieces/queen.rb
@@ -14,11 +14,18 @@ class Queen
   end
 
   def all_moves
+    straight + diagonal
+  end
+
+  def straight
     [(1..7).map { |n| [-n, 0] }.freeze,  # up
      (1..7).map { |n| [n, 0] }.freeze,   # down
      (1..7).map { |n| [0, -n] }.freeze,  # left
-     (1..7).map { |n| [0, n] }.freeze,   # right
-     (1..7).map { |n| [-n, -n] }.freeze, # up left
+     (1..7).map { |n| [0, n] }.freeze]   # right
+  end
+
+  def diagonal
+    [(1..7).map { |n| [-n, -n] }.freeze, # up left
      (1..7).map { |n| [-n, n] }.freeze,  # up right
      (1..7).map { |n| [n, -n] }.freeze,  # down left
      (1..7).map { |n| [n, n] }.freeze]   # down right
