2fcad286a16b1521054c6b0a66d2e1e7f82c6b5a
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-28 18:16:31 +0100

    code clean up

--- lib/modules/in_check.rb
+++ lib/modules/in_check.rb
@@ -1,8 +1,9 @@
 module InCheck
   def in_check?(color)
     king_location = locate_king(color)
-    potential_moves = all_available_moves(color)
-    puts "true" if potential_moves.any?(king_location)
+    potential_moves = all_available_moves(color).flatten(1)
+    puts 'true' if potential_moves.any?(king_location) # testing code - to be removed
+    return true if potential_moves.any?(king_location)
 
     false
   end
@@ -27,7 +28,7 @@ module InCheck
       else
         piece_class = piece_to_class(piece, position)
         piece_moves = piece_class.moves
-        piece_moves.each { |x, y| available_moves.push([(position[0] + x), (position[1] + y)]) }
+        available_moves.push(piece_moves)
       end
     end
     available_moves
@@ -37,11 +38,8 @@ module InCheck
     pos_array = []
     @board.each_with_index do |row, row_idx|
       row.each_with_index do |square, square_idx|
-        if @white_pieces.any?(square) && color == 'white'
-          pos_array << [row_idx, square_idx]
-        elsif @black_pieces.any?(square) && color == 'black'
-          pos_array << [row_idx, square_idx]
-        end
+        pos_array << [row_idx, square_idx] if @white_pieces.any?(square) && color == 'white'
+        pos_array << [row_idx, square_idx] if @black_pieces.any?(square) && color == 'black'
       end
     end
     pos_array
