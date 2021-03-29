705b0dfd91da1d3a3e4062d4905031e03d287981
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-03-29 17:47:51 +0100

    Save/Load info added to intro

--- lib/main.rb
+++ lib/main.rb
@@ -12,6 +12,8 @@ class Game
     puts 'Two players will take it in turn to select a piece, and the position they want to move it to.'
     puts 'When prompted, please enter the name of the piece you want to select (A reminder can be found in the README)'
     puts 'You will then be prompted again for a location to move the above piece to.'
+    puts "You can save at the beginning of your turn by typing 'save' "
+    puts "You can load this save again by typing 'load'"
   end
 end
 
